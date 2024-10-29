0e99d056c3f90da73c0d2ea9e240206382ea9c08
Vicent Roselló <vicentxeraco87@gmail.com>
2024-10-29 20:32:39 +0100

    Commit canvi ip del client flutter per a la conexió amb la api

--- app_torneig_flutter/lib/conf/ip.dart
+++ app_torneig_flutter/lib/conf/ip.dart
@@ -2,18 +2,5 @@
 
 //CONNEXIÓ AMB LA API I BD
 class Ip {
-  //ip AWS
-  //static const IP = "18.101.94.248";
-  //ip dockeritzada
-  static const IP = "apitorneig";
-  //ip casa
-  //static const IP = "192.168.0.17:9090";
-  //static const IP = "localhost";
-  //ip escola
-  //static const IP = "172.25.141.18:9090"; //PC consergeria
-  //static const IP = "172.25.141.239:9090"; //PC portatil
-  //ip institut
-  //static const IP = "10.2.0.29:9090";
-  //mobil
-  //static const IP = "192.168.45.107:9090";
+  static const IP = "localhost:9090";
 }
